756083a34599ab3cf3bdf20f3d2d0e7b1d6a1370
Peter Nagy <155898084+Skoteinos1@users.noreply.github.com>
2024-06-18 23:50:34 +0100

    Update image_tager.py

--- image_tager.py
+++ image_tager.py
@@ -5,3 +5,15 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 from tkinter import filedialog
 import shutil
+
+img_x_size = 1270
+img_y_size = 920
+
+def get_current_tags(img_path=''):
+    global current_img_tags
+
+    img_path = img_path.split('.')
+    if len(img_path) != 2:
+        print(' !!! TOO MANY DOTS IN FILENAME !!! ', img_path )
+        lbl_notif.config(text = '!!! TOO MANY DOTS IN FILENAME !!!')
+        return
